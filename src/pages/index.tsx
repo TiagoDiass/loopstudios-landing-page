36380e5ae68adde6ee93907e143464f50d7035d2
Tiago Dias <tiago.costadiasss@gmail.com>
2021-05-12 14:42:02 -0300

    feat: add title metatag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,19 @@
+import Head from 'next/head';
 import Header from '../components/Header/Header';
 import * as S from '../styles/pages/home.styles';
 
 export default function Home() {
   return (
-    <S.MainContainer>
-      <S.HeroBanner>
-        <Header />
-        <h1>IMMERSIVE EXPERIENCES THAT DELIVER</h1>
-      </S.HeroBanner>
-    </S.MainContainer>
+    <>
+      <Head>
+        <title>Loopstudios | Home</title>
+      </Head>
+      <S.MainContainer>
+        <S.HeroBanner>
+          <Header />
+          <h1>IMMERSIVE EXPERIENCES THAT DELIVER</h1>
+        </S.HeroBanner>
+      </S.MainContainer>
+    </>
   );
 }
