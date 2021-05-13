97911cc7ad78334d08b22dfab85feb4c9836309d
Tiago Dias <tiago.costadiasss@gmail.com>
2021-05-13 13:26:29 -0300

    feat: add footer

---
+++ src/content/socialMedia.ts
@@ -0,0 +1,18 @@
+export default [
+  {
+    title: 'Our Facebook',
+    imageName: 'icon-facebook.svg',
+  },
+  {
+    title: 'Our Twitter',
+    imageName: 'icon-twitter.svg',
+  },
+  {
+    title: 'Our Pinterest',
+    imageName: 'icon-pinterest.svg',
+  },
+  {
+    title: 'Our Instagram',
+    imageName: 'icon-instagram.svg',
+  },
+];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import creations from '../content/creations';
+import socialMedia from '../content/socialMedia';
 import Header from '../components/Header/Header';
 import * as S from '../styles/pages/home.styles';
 
@@ -56,7 +57,37 @@ export default function Home() {
           </div>
         </S.CreationsSection>
 
-        <div style={{ height: '25rem', background: 'tomato' }}></div>
+        <S.Footer>
+          <div className='content'>
+            <div className='leftBlock'>
+              <img src='/images/logo.svg' alt='Loopstudios' />
+              <nav>
+                <a href='#'>About</a>
+                <a href='#'>Careers</a>
+                <a href='#'>Events</a>
+                <a href='#'>Products</a>
+                <a href='#'>Support</a>
+              </nav>
+            </div>
+
+            <div className='rightBlock'>
+              <ul className='socialMediaLinks'>
+                {socialMedia.map((socialMedia, index) => (
+                  <li key={index}>
+                    <a href='#' title={socialMedia.title}>
+                      <img
+                        src={`/images/${socialMedia.imageName}`}
+                        alt={socialMedia.title}
+                      />
+                    </a>
+                  </li>
+                ))}
+              </ul>
+
+              <p>© 2021 Loopstudios. All rights reserved.</p>
+            </div>
+          </div>
+        </S.Footer>
       </S.MainContainer>
     </>
   );
